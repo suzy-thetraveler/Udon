694684e4b542b433f3ba7865074357eecd67353a
suzy-thetraveler <suzythetraveler@khu.ac.kr>
2021-10-23 20:48:25 +0900

    [2021-10-23 이수진 FE migration중]

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -19,90 +19,90 @@ public class HomeController {
 	@RequestMapping(value = "/myclub", method = RequestMethod.GET)
 	public String myclub() {
 
-		return "myclub";
+		return "content/myclub";
 	}
 
 	// 클럽 페이지
 	@RequestMapping(value = "/club_detail", method = RequestMethod.GET)
 	public String club_detail() {
 
-		return "club_detail";
+		return "content/club_detail";
 	}
 
 	// 클럽 앨범
 	@RequestMapping(value = "/club_album", method = RequestMethod.GET)
 	public String club_album() {
 
-		return "club_album";
+		return "content/club_album";
 	}
 
 	// 클럽 일정
 	@RequestMapping(value = "/club_calendar", method = RequestMethod.GET)
 	public String club_calendar() {
 
-		return "club_calendar";
+		return "content/club_calendar";
 	}
 
 	@RequestMapping(value = "/club_vote", method = RequestMethod.GET)
 	public String club_vote() {
 
-		return "club_vote";
+		return "content/club_vote";
 	}
 
 	// 클럽 멤버 찾기, 메세지 기능
 	@RequestMapping(value = "/club_members", method = RequestMethod.GET)
 	public String club_members() {
 
-		return "club_members";
+		return "content/club_members";
 	}
 
 	// 클럽 만들기 대분류선택
 	@RequestMapping(value = "/create_club1", method = RequestMethod.GET)
 	public String create_club1() {
 
-		return "create_club1";
+		return "content/create_club1";
 	}
 
 	// 클럽 만들기 상세정보입력
 	@RequestMapping(value = "/create_club2", method = RequestMethod.GET)
 	public String create_club2() {
 
-		return "create_club2";
+		return "content/create_club2";
 	}
 
 	// 클럽 회원 모집 페이지
 	@RequestMapping(value = "/club_recruit", method = RequestMethod.GET)
 	public String club_recruit() {
 
-		return "club_recruit";
+		return "content/club_recruit";
 	}
 
 	// 클럽 회원 모집 게시판
 	@RequestMapping(value = "/recruit_board", method = RequestMethod.GET)
 	public String recruit_board() {
 
-		return "recruit_board";
+		return "content/recruit_board";
 	}
 
 	// 클럽 회원 모집 글
 	@RequestMapping(value = "/recruit_detail", method = RequestMethod.GET)
 	public String recruit_detail() {
 
-		return "recruit_detail";
+		return "content/recruit_detail";
 	}
 
 	// 클럽 지원한 회원들 보기
-	@RequestMapping(value = "/apply_members", method = RequestMethod.GET)
+	@RequestMapping(value = "/apply_member", method = RequestMethod.GET)
 	public String apply_members() {
 
-		return "apply_members";
+		return "content/apply_member";
 	}
 
 	// 로그인 페이지
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 
-		return "login";
+		return "content/login";
 
 	}
 
@@ -110,77 +110,77 @@ public class HomeController {
 	@RequestMapping(value = "/register", method = RequestMethod.GET)
 	public String register() {
 
-		return "register";
+		return "content/register";
 	}
 
 	// 아이디, 비밀번호 찾기
 	@RequestMapping(value = "/find_account", method = RequestMethod.GET)
 	public String find_account() {
 
-		return "find_account";
+		return "content/find_account";
 	}
 
 	// 마이 페이지
 	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
 	public String mypage() {
 
-		return "mypage";
+		return "content/mypage";
 	}
 
 	// 게시글 상세
 	@RequestMapping(value = "/detail_board", method = RequestMethod.GET)
 	public String detail_board() {
 
-		return "detail_board";
+		return "content/detail_board";
 	}
 
 	// 커뮤니티 상세
 	@RequestMapping(value = "/community_detail", method = RequestMethod.GET)
 	public String community_detail() {
 
-		return "community_detail";
+		return "content/community_detail";
 	}
 
 	// 커뮤니티
 	@RequestMapping(value = "/community", method = RequestMethod.GET)
 	public String community() {
 
-		return "community";
+		return "content/community";
 	}
 
 	// 글쓰기
 	@RequestMapping(value = "/write_board", method = RequestMethod.GET)
 	public String write_board() {
 
-		return "write_board";
+		return "content/write_board";
 	}
 
 	// 회원정보
 	@RequestMapping(value = "/myinfo", method = RequestMethod.GET)
 	public String myinfo() {
 
-		return "myinfo";
+		return "content/myinfo";
 	}
 
 	// 프로필 수정
 	@RequestMapping(value = "/setprofile", method = RequestMethod.GET)
 	public String setprofile() {
 
-		return "setprofile";
+		return "content/setprofile";
 	}
 
 	// 고객센터
 	@RequestMapping(value = "/cs_center", method = RequestMethod.GET)
 	public String cs_center() {
 
-		return "cs_center";
+		return "content/cs_center";
 	}
 
 	// 내가 쓴 글 목록
 	@RequestMapping(value = "/mypostings", method = RequestMethod.GET)
 	public String mypostings() {
 
-		return "mypostings";
+		return "content/mypostings";
 	}
 
 }
